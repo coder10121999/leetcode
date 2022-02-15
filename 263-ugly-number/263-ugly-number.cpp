201bf6e26b6b70a652ec5877a132362c23473265
Devendu Tiwari <43497531+coder10121999@users.noreply.github.com>
2022-02-15 22:53:16 +0530

    Time: 17 ms (5.01%), Space: 5.8 MB (87.05%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        
+        if(n==0)
+            return false;
+        
+        while(n%2==0)
+            n/=2;
+        
+        cout<<n<<"\n";
+        while(n%3==0)
+            n/=3;
+        
+        cout<<n<<"\n";
+        while(n%5==0)
+            n/=5;
+        
+        cout<<n<<"\n";
+        if(n==1)
+            return true;
+        
+        return false;
+        
+    }
+};
